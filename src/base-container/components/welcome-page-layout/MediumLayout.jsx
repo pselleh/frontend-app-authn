ba6a42a0150a5e1c75a263d26b80183e879003d7
Phil Selleh <pselleh@gmail.com>
2026-07-23 01:14:03 +0000

    Remove stock logos from authn welcome layouts

--- src/base-container/components/welcome-page-layout/MediumLayout.jsx
+++ src/base-container/components/welcome-page-layout/MediumLayout.jsx
@@ -15,9 +15,7 @@ const MediumLayout = ({ fullName }) => {
       <div className="w-100 medium-screen-top-stripe" />
       <div className="w-100 p-0 mb-3 d-flex">
         <div className="col-md-10 bg-light-200">
-          <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-            <Image className="logo" alt={getConfig().SITE_NAME} src={getConfig().LOGO_URL} />
-          </Hyperlink>
+          {/* Logo rendered by CbaAuthnChrome */}
           <div className="d-flex align-items-center justify-content-center mb-4 ml-5">
             <div className="medium-yellow-line mt-5 mr-n2" />
             <div>
