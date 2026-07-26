6fb48eba5d587d4fb9b6b4e4a2d010669adb7ce7
Phil Selleh <pselleh@gmail.com>
2026-07-26 01:21:33 +0000

    Add Google reCAPTCHA Enterprise widgets to auth MFE

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -6,7 +6,11 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import CbaAuthnChrome from './CbaAuthnChrome';
 import {
-  EmbeddedRegistrationRoute, NotFoundPage, registerIcons, UnAuthOnlyRoute, Zendesk,
+  EmbeddedRegistrationRoute,
+  NotFoundPage,
+  registerIcons,
+  UnAuthOnlyRoute,
+  Zendesk,
 } from './common-components';
 import configureStore from './data/configureStore';
 import {
@@ -31,6 +35,7 @@ import './index.scss';
 import './styles.scss';
 
 registerIcons();
+
 const RECAPTCHA_SITE_KEY = '6LfBVV0tAAAAAI2M3K2FFcMz2tbLZ2EXgkAo81G_';
 
 const buildFaviconUrl = () => {
@@ -38,40 +43,111 @@ const buildFaviconUrl = () => {
   if (configured) {
     return configured;
   }
+
   const lmsBaseUrl = (getConfig().LMS_BASE_URL || '').replace(/\/$/, '');
-  return lmsBaseUrl ? `${lmsBaseUrl}/theming/asset/images/favicon.ico` : '/theming/asset/images/favicon.ico';
+
+  return lmsBaseUrl
+    ? `${lmsBaseUrl}/theming/asset/images/favicon.ico`
+    : '/theming/asset/images/favicon.ico';
 };
 
-const MainApp = () => (
-  <AppProvider store={configureStore()}>
-    <CbaAuthnChrome>
-      <Helmet>
-        <link rel="shortcut icon" href={buildFaviconUrl()} type="image/x-icon" />
-        <script src={`https://www.google.com/recaptcha/enterprise.js?render=explicit&sitekey=${RECAPTCHA_SITE_KEY}`} />
-      </Helmet>
-      {getConfig().ZENDESK_KEY && <Zendesk />}
-      <Routes>
-        <Route path="/" element={<Navigate replace to={updatePathWithQueryParams(REGISTER_PAGE)} />} />
-        <Route
-          path={REGISTER_EMBEDDED_PAGE}
-          element={<EmbeddedRegistrationRoute><RegistrationPage /></EmbeddedRegistrationRoute>}
-        />
-        <Route
-          path={LOGIN_PAGE}
-          element={
-            <UnAuthOnlyRoute><Logistration selectedPage={LOGIN_PAGE} /></UnAuthOnlyRoute>
-          }
-        />
-        <Route path={REGISTER_PAGE} element={<UnAuthOnlyRoute><Logistration /></UnAuthOnlyRoute>} />
-        <Route path={RESET_PAGE} element={<UnAuthOnlyRoute><ForgotPasswordPage /></UnAuthOnlyRoute>} />
-        <Route path={PASSWORD_RESET_CONFIRM} element={<ResetPasswordPage />} />
-        <Route path={AUTHN_PROGRESSIVE_PROFILING} element={<ProgressiveProfiling />} />
-        <Route path={RECOMMENDATIONS} element={<RecommendationsPage />} />
-        <Route path={PAGE_NOT_FOUND} element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate replace to={PAGE_NOT_FOUND} />} />
-      </Routes>
-    </CbaAuthnChrome>
-  </AppProvider>
-);
+const MainApp = () => {
+  // TEMPORARY: Verify runtime configuration
+  console.log('MFE config:', getConfig());
+  console.log('RECAPTCHA_SITE_KEY:', getConfig().RECAPTCHA_SITE_KEY);
+
+  return (
+    <AppProvider store={configureStore()}>
+      <CbaAuthnChrome>
+        <Helmet>
+          <link
+            rel="shortcut icon"
+            href={buildFaviconUrl()}
+            type="image/x-icon"
+          />
+
+          <script
+            src={`https://www.google.com/recaptcha/enterprise.js?render=explicit&sitekey=${RECAPTCHA_SITE_KEY}`}
+          />
+        </Helmet>
+
+        {getConfig().ZENDESK_KEY && <Zendesk />}
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Navigate
+                replace
+                to={updatePathWithQueryParams(REGISTER_PAGE)}
+              />
+            }
+          />
+
+          <Route
+            path={REGISTER_EMBEDDED_PAGE}
+            element={
+              <EmbeddedRegistrationRoute>
+                <RegistrationPage />
+              </EmbeddedRegistrationRoute>
+            }
+          />
+
+          <Route
+            path={LOGIN_PAGE}
+            element={
+              <UnAuthOnlyRoute>
+                <Logistration selectedPage={LOGIN_PAGE} />
+              </UnAuthOnlyRoute>
+            }
+          />
+
+          <Route
+            path={REGISTER_PAGE}
+            element={
+              <UnAuthOnlyRoute>
+                <Logistration />
+              </UnAuthOnlyRoute>
+            }
+          />
+
+          <Route
+            path={RESET_PAGE}
+            element={
+              <UnAuthOnlyRoute>
+                <ForgotPasswordPage />
+              </UnAuthOnlyRoute>
+            }
+          />
+
+          <Route
+            path={PASSWORD_RESET_CONFIRM}
+            element={<ResetPasswordPage />}
+          />
+
+          <Route
+            path={AUTHN_PROGRESSIVE_PROFILING}
+            element={<ProgressiveProfiling />}
+          />
+
+          <Route
+            path={RECOMMENDATIONS}
+            element={<RecommendationsPage />}
+          />
+
+          <Route
+            path={PAGE_NOT_FOUND}
+            element={<NotFoundPage />}
+          />
+
+          <Route
+            path="*"
+            element={<Navigate replace to={PAGE_NOT_FOUND} />}
+          />
+        </Routes>
+      </CbaAuthnChrome>
+    </AppProvider>
+  );
+};
 
 export default MainApp;
